62885ff698768e0d3f30eb7f62bdceeba452694c
Tong Wang <tongwang@Tongs-MacBook-Pro.local>
2018-06-12 17:33:14 -0500

    This is the modification of the file

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -8,12 +8,3 @@ int main()
     
     return 0;
 }
-
-// #include <iostream> // define std::cout etc.
-//  
-// int main()
-// {
-//     std::cout << "Hola, mundo!\n";
-// 	std::cout << "Hola otra vez" << std::endl;
-// 	// NB: En C++, ';' ES IMPORTANTISIMO!
-// }
