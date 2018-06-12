931f6790647400329b623c1e897dc8e0a540b5bd
saac <sergio.alcala@ciencias.unam.mx>
2018-06-12 17:26:31 -0500

    This is a classical hello world in C++

---
+++ HelloWorld.cpp
@@ -0,0 +1,19 @@
+#include <iostream> // define std::cout etc.
+ 
+int main()
+{
+
+
+    std::cout << "Hello \n";
+    
+    return 0;
+}
+
+// #include <iostream> // define std::cout etc.
+//  
+// int main()
+// {
+//     std::cout << "Hola, mundo!\n";
+// 	std::cout << "Hola otra vez" << std::endl;
+// 	// NB: En C++, ';' ES IMPORTANTISIMO!
+// }
